ab81da9a9b21c3ff93725d881488bbca519988a5
AsmaaTarek <asmaatarek93@gmail.com>
2019-06-12 08:27:40 +0200

    add search bar

--- Aziz Maraka Music App/Controllers/UI/SongsTableViewController.swift
+++ Aziz Maraka Music App/Controllers/UI/SongsTableViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class SongsTableViewController: UIViewController {
     @IBOutlet weak var songsTable: UITableView!
+ 
+    @IBOutlet weak var searchBarr: UISearchBar!
     var songsList = NSArray()
+    var songNames = [String]()
+    var totalSongsArr = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         songsTable.tableFooterView = UIView()
         // Do any additional setup after loading the view.
         readFromPlist()
+        convertArrayToString()
     }
     
     func readFromPlist(){
         let path = Bundle.main.path(forResource: "SongsList", ofType: "plist")
         songsList = NSArray(contentsOf: URL(fileURLWithPath: path ?? "" )) ?? []
+    }
+    func convertArrayToString(){
+        for index in 0..<songsList.count{
+        let currentSong = songsList[index] as! [String:String]
+        let currentSongName = currentSong["SongName"]
+       songNames.append(currentSongName ?? "")
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -44,15 +56,34 @@ extension SongsTableViewController: UITableViewDelegate, UITableViewDataSource{
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return songsList.count
+        return songNames.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: songTableViewCellID, for: indexPath)
-        let currentSong = songsList[indexPath.row] as! [String:String]
-        let currentSongName = currentSong["SongName"]
-        cell.textLabel?.text = currentSongName
+        let currentSong = songNames[indexPath.row]
+        
+        cell.textLabel?.text = currentSong
         return cell
     }
 }
+
+extension SongsTableViewController :  UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            songNames = []
+            convertArrayToString()
+            songsTable.reloadData()
+        return
+        }
+          print(songNames)
+        print (songsList)
+            songNames = songNames.filter { (song) -> Bool in
+                guard let text = searchBarr.text else {return false}
+              return song.lowercased().contains(text.lowercased())
+                }
+                songsTable.reloadData()
+            }
+        }
+
 
 
